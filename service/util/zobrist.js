5882cb3c7f96c870fcc55b0d4227766dae457aa1
justwiner <392110917@qq.com>
2019-02-19 15:05:14 +0800

    完成置换表

--- service/util/zobrist.js
+++ service/util/zobrist.js
@@ -1,11 +1,14 @@
 const R = require('./role')
 const Random = require("random-js")
 
+// 置换表hash算法，用hash值表示当前棋局
+
 let Zobrist = function (size) {
     // 设置棋盘尺寸
     this.size = size || 15;
 }
 
+// 初始化自己和对手的hash值
 Zobrist.prototype.init = function () {
     this.com = [];
     this.hum = [];
@@ -17,12 +20,13 @@ Zobrist.prototype.init = function () {
     this.code = this._rand();
 }
 
+// 获取高质量随机数，确保完全不重复
 let engine = Random.engines.mt19937().autoSeed()
-
 Zobrist.prototype._rand = function () {
     return Random.integer(1, 1000000000)(engine); //再多一位就溢出了。。
 }
 
+// 通过异或操作记录棋局
 Zobrist.prototype.go = function (x, y, role) {
     let index = this.size * x + y;
     this.code ^= (role == R.com ? this.com[index] : this.hum[index]);
