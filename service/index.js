a31da0aec707e233c6e71d07b53cc5fe98d4258d
justwiner <392110917@qq.com>
2019-02-28 15:30:54 +0800

    获取接口中的角色信息，并用于计算

--- service/index.js
+++ service/index.js
@@ -1,11 +1,11 @@
 const think = require('./negamax')
 const board = require('./board')
 const config = require('./util/config')
+const Role = require('./util/role')
 
 function IThink ({ chessRecords, spec, array, role }) {
-    
     board.init(array)
-    let result = think(undefined, config.searchDeep)
+    let result = think(role, config.searchDeep, )
     return {
         index: {
             mulX: result[1],
